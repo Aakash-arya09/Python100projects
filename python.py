c94293d29c06451d93c39a5ea7010d9474d52fdc
Aakash Arya <aakashkumararya458@gmail.com>
2026-08-18 09:23:51 +0530

    first-commit

---
+++ python.py
@@ -0,0 +1,9 @@
+a = int(input("input the number:"))
+b = int(input("input the number:"))
+if a > b:
+    print("a is greater than b")
+elif a < b:
+    print("a is less than b")
+else:
+    print("a is equal to b")
+    
