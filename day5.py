0e3e29acc822d6a317dc93c6dea0f3264caf11a0
Aakash Arya <aakashkumararya458@gmail.com>
2026-08-21 19:54:13 +0530

    day5

--- day5.py
+++ day5.py
@@ -35,13 +35,15 @@ import time
 start = int(input("Enter the number to start the countdown from: "))
 
 # Step 2: Countdown using a while loop
+
 print("\n--- Countdown Begins ---")
 while start > 0:
   print(start)
   time.sleep(1)
   start -= 1
 
-# Step 3: Print final message
+# Step 3: Print final message 
+
 print("Countdown Complete!")
 
 
