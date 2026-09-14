9e18a2c16df7c4827c82b7e090ef2094c7407675
Aakash Arya <aakashkumararya458@gmail.com>
2026-09-14 22:29:10 +0530

    day hold

--- python.py
+++ python.py
@@ -20,3 +20,6 @@ def name(user):
     print(f"Hello, {user}! How are You .")
     
 name("ramu")
+
+
+#hello, this is a simple Python program that demonstrates basic concepts such as printing text, using variables, performing addition, and using an if statement. It also includes a function that takes a user's name as input and prints a personalized greeting.
